5a9559a555c0928e7a2cb059a5571e52b3271c37
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-12-30 19:51:45 +0530

    Newletter Form Added

--- src/components/Widget/Newsletter.jsx
+++ src/components/Widget/Newsletter.jsx
@@ -6,9 +6,9 @@ export default function Newsletter({title, subtitle, placeholder}) {
     <>
       {title && <h2 className="cs-widget_title">{title}</h2>}
       <Div className="cs-newsletter cs-style1">
-        <form action="#" className="cs-newsletter_form">
-          <input type="email" className="cs-newsletter_input" placeholder={placeholder} />
-          <button className="cs-newsletter_btn"><span>Send</span></button>
+      <form action="https://send.pageclip.co/mNLKo9qap4t0tePIZMPiqJwmYXWP3hsU/newsletter" class="pageclip-form" method="post">
+          <input name='email' type="email" className="cs-newsletter_input" placeholder={placeholder} />
+          <button type='submit' className="cs-newsletter_btn"><span>Send</span></button>
         </form>
         <Div className="cs-newsletter_text">{subtitle}</Div>
       </Div>
